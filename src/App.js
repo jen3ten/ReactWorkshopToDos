e20244184bd4c53f9f9caadfd975910ac2074826
Jen E <jen@wecancodeit.org>
2018-12-19 10:57:06 -0500

    Added some AJAX practice in React

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ class App extends Component {
     super();
     this.state = {
       todos: ["A", "B", "C"],
-      currentText: ''
+      currentText: '',
+      isHoliday: null
     }
+  }
+
+  //To retrieve data from server use AJAX here:
+  //Use fetch and chain of promises
+  //Google how to use fetch for syntax
+  //This should go in its own russianHolidays.js file in which a date is passed in and the return is
+  //  the result of the AJAX, 
+  //  export default (date) => {return fetch etc...}
+  //  It is not component related so it should be put outside of component
+  componentWillMount(){
+    fetch('https://datazen.katren.ru/calendar/day/2018-06-11/')
+      .then(res => res.json())
+      .then(data => { //convert response to an object
+        console.log(data);
+        this.setState({isHoliday : data.holiday})
+      });
   }
 
   //Allows typing in text box and keeps state in sync
@@ -25,13 +42,14 @@ class App extends Component {
   render() {
     //const todos = ["Do Stuff", "Do more stuff", "You forgot to do this"];
     // const todoList = todos.map(t => <li key={t}>{t}</li>);
-    const {currentText, todos} = this.state;
+    const {currentText, todos, isHoliday} = this.state;
     return (
       <Todos
         currentText = {currentText}
         setText = {this.setText}
         addToDo = {this.addToDo}
         todos = {todos}
+        isHoliday = {isHoliday}
       />
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -5,15 +5,17 @@ import AddBar from './AddBar';
 
 class Todos extends Component {
     render() {
-        const {currentText, setText, addToDo, todos} = this.props;
+        const {currentText, setText, addToDo, todos, isHoliday} = this.props;
          return (
             <div>
-            <AddBar 
-              currentText = {currentText}
-              setText = {setText}
-              addToDo = {addToDo}
-            />
-            <ToDoList todos={todos} />
+                <AddBar 
+                currentText = {currentText}
+                setText = {setText}
+                addToDo = {addToDo}
+                />
+                <ToDoList todos={todos} />
+                <h4>This is just a little experiment with an AJAX call:</h4>
+                <div>Is it a holiday? {isHoliday ? "Yes" : "No"}</div>
           </div>
             );
     }
